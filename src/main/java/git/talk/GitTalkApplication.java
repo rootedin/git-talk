c8452d854bd7d5fb273474cc91cedb9ba709d39b
rootedin <seonggn.yun@gmail.com>
2020-06-07 17:39:35 +0900

    backend init

---
+++ src/main/java/git/talk/GitTalkApplication.java
@@ -0,0 +1,13 @@
+package git.talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/talk/GitTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package git.talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
